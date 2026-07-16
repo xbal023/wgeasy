9ca1baa007e0fb13a872a2e56f16abe5e7d21413
xbal023 <iqbalsheila619@gmail.com>
2026-07-16 12:45:57 +0700

    fix: robustly parse ADMIN_TELEGRAM_IDS to ignore quotes and non-numeric characters

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const envSchema = z.object({
   BOT_NAME: z.string(),
   BOT_TOKEN: z.string(),
-  ADMIN_TELEGRAM_IDS: z.string().transform((val) => val.split(',').map(Number)),
+  ADMIN_TELEGRAM_IDS: z.string().transform((val) => val.split(',').map(s => Number(s.replace(/[^0-9]/g, ''))).filter(n => !isNaN(n) && n > 0)),
   DATABASE_URL: z.string().url(),
   PORT: z.string().default('3000'),
   NODE_ENV: z.enum(['development', 'production']).default('development'),
